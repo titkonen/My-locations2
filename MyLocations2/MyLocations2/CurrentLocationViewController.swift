9c1eebb65ccce092074e7c521433217e430b09bd
titko <toni.itkonen@icloud.com>
2019-02-24 17:27:56 +0200

    Initial commit

---
+++ MyLocations2/MyLocations2/CurrentLocationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations2
+//
+//  Created by Toni Itkonen on 24/02/2019.
+//  Copyright © 2019 Toni Itkonen. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
