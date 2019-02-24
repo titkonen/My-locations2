ea76b56894fdc6e6e81d6e4afe033c8e2aeb1409
titko <toni.itkonen@icloud.com>
2019-02-24 17:35:51 +0200

    p. 522 ohjeissa

--- MyLocations2/MyLocations2/CurrentLocationViewController.swift
+++ MyLocations2/MyLocations2/CurrentLocationViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    
+    // MARK:- Actions
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
