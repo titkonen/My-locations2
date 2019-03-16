165b80e269d9fd17e3d8eb7d4de6795e16394034
titko <toni.itkonen@icloud.com>
2019-03-16 13:27:26 +0200

    Chapter 26 end. Clean build and works OK.

--- MyLocations3/MyLocations/LocationDetailsViewController.swift
+++ MyLocations3/MyLocations/LocationDetailsViewController.swift
@@ -37,12 +37,50 @@ class LocationDetailsViewController: UITableViewController {
       addressLabel.text = "No Address Found"
     }
     dateLabel.text = format(date: Date())
+    
+    // Hide keyboard
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+    gestureRecognizer.cancelsTouchesInView = false
+    tableView.addGestureRecognizer(gestureRecognizer)
+  }
+  
+  @objc func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer) {
+    let point = gestureRecognizer.location(in: tableView)
+    let indexPath = tableView.indexPathForRow(at: point)
+    
+    if indexPath != nil && indexPath!.section == 0
+      && indexPath!.row == 0 {
+      return
+    }
+    descriptionTextView.resignFirstResponder()
+  }
+  
+  // MARK:- Table View Delegates
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    if indexPath.section == 0 || indexPath.section == 1 {
+      return indexPath
+    } else {
+      return nil
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.section == 0 && indexPath.row == 0 {
+      descriptionTextView.becomeFirstResponder()
+    }
   }
   
   // MARK: - Actions
   @IBAction func done() {
-    navigationController?.popViewController(animated: true)
+    let hudView = HudView.hud(inView: navigationController!.view, animated: true)
+    
+    hudView.text = "Tagged"
+    afterDelay(0.6) {
+      hudView.hide() // piilottaa check mark modalin
+      self.navigationController?.popViewController(animated: true)
+    }
   }
+  
   
   @IBAction func cancel() {
     navigationController?.popViewController(animated: true)
